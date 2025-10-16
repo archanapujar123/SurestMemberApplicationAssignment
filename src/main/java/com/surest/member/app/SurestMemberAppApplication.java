2315c80e7588982f4ac11bbff122e238f8641b74
archanapujar123 <archanapujar27@gmail.com>
2025-10-16 14:28:47 +0530

    Initial Base Code with Configuration

---
+++ src/main/java/com/surest/member/app/SurestMemberAppApplication.java
@@ -0,0 +1,15 @@
+package com.surest.member.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class SurestMemberAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurestMemberAppApplication.class, args);
+	}
+
+}
